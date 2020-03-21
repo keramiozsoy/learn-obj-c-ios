61b17802b621c17bc37c3315bf77ec0c9d534574
kerami <keramiozsoy@hotmail.com>
2020-03-22 01:06:30 +0300

    lesson 1

--- learn-obj-c-ios/ViewController.m
+++ learn-obj-c-ios/ViewController.m
@@ -13,11 +13,33 @@
 @end
 
 @implementation ViewController
+@synthesize label1;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+
+- (IBAction)btnTap:(id)sender {
+    
+    NSLog(@" Log - Butona Basildi");
+    
+    // [_label1 setText:@"Butona Basildi"];
+    
+    /*
+     Tanımlamalar private
+     olduğundan
+     @synthesize tanımlaması
+     yapıldıktan sonra _
+     işareti kullanılmadan
+     erişim sağlanabilir.
+     */
+    [label1 setText:@"Butona Basildi"];
+    
+}
+
 
 
 @end

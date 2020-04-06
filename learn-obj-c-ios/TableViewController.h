33cb74da48497384bf508a4ad8bea85430607216
kerami <keramiozsoy@hotmail.com>
2020-04-06 21:19:10 +0300

    lesson 15

--- learn-obj-c-ios/TableViewController.h
+++ learn-obj-c-ios/TableViewController.h
@@ -1,18 +1,21 @@
 //
-//  MyFirstViewController.h
+//  TableViewController.h
 //  learn-obj-c-ios
 //
-//  Created by Mac on 3.04.2020.
+//  Created by Mac on 6.04.2020.
 //  Copyright © 2020 Mac. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-#import "MySecondViewController.h"
-
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MyFirstViewController : UIViewController
+@interface TableViewController : UITableViewController
+
+
+
+@property NSArray *arr1;
+
 
 @end
 
